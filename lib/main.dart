4aa5a4eb0ff441d024388f80713718963e4a7b97
zain97699 <142928813+zain97699@users.noreply.github.com>
2023-09-24 22:36:08 +0500

    provider provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:fav_app/pp';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +12,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    return ChangeNotifierProvider(create: (_)=>counterprovider() )
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
---
+++ provider/count_provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/foundation.dart';
+import 'package:provider/provider.dart';
+
+
+
+class countProvider with ChangeNotifier
+{
+
+
+
+void counterprovider(){
+
+}
+}
