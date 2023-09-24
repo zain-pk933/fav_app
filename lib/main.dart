79453ca6d6f57c71f8314fe393a137b0322f2b3e
Zain Sajjad <zaingaming933@gmail.com>
2023-09-25 00:32:38 +0500

    provider_setstate

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:fav_app/provider/count_provider.dart';
+
+import 'provider/count_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:fav_app/pp';
+
+
+
 
 void main() {
   runApp(const MyApp());
@@ -13,15 +18,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    return ChangeNotifierProvider(create: (_)=>counterprovider() )
-    return MaterialApp(
+    return ChangeNotifierProvider(create: (_) => countProvider(),child:MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    )  );
+    
   }
 }
 
@@ -35,27 +40,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  // int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  // void _incrementCounter() {
+  //   setState(() {
+  //     _counter++;
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+     final countsProvider=Provider.of<countProvider>(context);
+     print("bulild");
+     return Scaffold( appBar: 
+     AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
@@ -84,17 +82,18 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              countsProvider.count.toString(),
               style: Theme.of(context).textTheme.headlineMedium,
             ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+            FloatingActionButton(
+        onPressed: (){countsProvider.counterprovider();},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+          ],
+        ),
+      
+),       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- lib/provider/count_provider.dart
+++ lib/provider/count_provider.dart
@@ -6,9 +6,12 @@ import 'package:provider/provider.dart';
 class countProvider with ChangeNotifier
 {
 
+int _count = 0;
+int get count=>_count;
 
 
 void counterprovider(){
-
+  _count++ ;
+notifyListeners();
 }
 }
